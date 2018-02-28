5ea7d43e5b3c8e12d005c76e8b5e5c697007c84c
knbbl <36883064+knbbl@users.noreply.github.com>
2018-02-28 11:03:43 +0100

    Update OPCuaGetDeviceList.py

--- OPCuaGetDeviceList.py
+++ OPCuaGetDeviceList.py
@@ -1,29 +1,26 @@
 from opcua import ua
 from opcua import Client
-
-def connectClient(url):
-    client = Client(url)
-    client.connect()
-    return client
-
-def disconnectClient(client):
-    client.disconnect()
         
 if __name__ == '__main__':
     
-    # connect client (replace xx.xx.xx.xx with the IP address of your server)
-    client = connectClient("opc.tcp://xx.xx.xx.xx:4840/freeopcua/server/")
+    # create client (replace xx.xx.xx.xx with the IP address of your server)
+    client = Client("opc.tcp://xx.xx.xx.xx:4840/freeopcua/server/")
     
-    # get device list and print result
-    path = [ua.QualifiedName(name = 'Objects', namespaceidx = 0)]
-    uaNode = client.get_root_node().get_child(path)
-    nodes = uaNode.get_children()
-    deviceList = []
-    for n in nodes:
-        name = n.get_display_name().Text
-        if name != 'Server':
-            deviceList.append(name)
-    print(deviceList)
+    try:
+        # connect client
+        client.connect()        
+        
+        # get device list and print result
+        path = [ua.QualifiedName(name = 'Objects', namespaceidx = 0)]
+        uaNode = client.get_root_node().get_child(path)
+        nodes = uaNode.get_children()
+        deviceList = []
+        for n in nodes:
+            name = n.get_display_name().Text
+            if name != 'Server':
+                deviceList.append(name)
+        print(deviceList)
     
-    # disconnect client
-    disconnectClient(client)
+    finally:
+        # disconnect client
+        client.disconnect()
