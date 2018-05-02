f758d8c309a9e7d27cd5f3bfca60c02313cc79b3
knbbl <36883064+knbbl@users.noreply.github.com>
2018-05-02 18:36:01 +0200

    Update OPCuaGetVibrationData.py

--- examples/OPCuaGetVibrationData.py
+++ examples/OPCuaGetVibrationData.py
@@ -5,73 +5,8 @@ from opcua import Client
 
 import matplotlib.pyplot as plt
     
+
 def readHistory(macId, browseName = None, startTime = None, endTime = None, numValues = 8192):
-    
-    # get variable handle
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = browseName, namespaceidx = nsIdx))  
-    uaNode = client.get_root_node().get_child(bpath)
-    
-    # read history from variable
-    dates, values, starttime, endtime = ([], [], startTime, endTime)
-    # In each iteration, 1024 samples are extracted.
-    while numValues > 0:
-        if starttime and endtime and starttime > endtime:
-            break
-        historyReadResult = readRawHistory(
-            uaClient = client.uaclient,
-            uaNodeId = uaNode.nodeid,
-            starttime = starttime,
-            endtime = endtime,
-            numvalues = min(numValues, 1024)
-        )
-        dvArr = historyReadResult.HistoryData.DataValues
-        if len(dvArr) == 0:
-            break
-        numValues -= len(dvArr)
-        for sample in dvArr:
-            dates.append(pytz.utc.localize(sample.SourceTimestamp))
-            values.append(sample.Value.Value)
-        # Set new endtime to 'oldest time stamp' minus 1us. For opcua history, timestamps
-        # used in read_raw_history(...) are ServerTimestamp, not SourceTimestamp!
-        if starttime is None:
-            # read_raw_history returns [oldest..newest]. Continue reading at 'newest - 1us'
-            endtime = pytz.utc.localize(dvArr[-1].ServerTimestamp) - datetime.timedelta(microseconds=1)
-        else:
-            # read_raw_history returns [newest..oldest]. Continue reading at 'oldest + 1us'
-            starttime = pytz.utc.localize(dvArr[-1].ServerTimestamp) + datetime.timedelta(microseconds=1)
-    # If no starttime is given, results of read_raw_history are reversed.
-    if starttime is None:
-        values.reverse()
-        dates.reverse()
-    return (values, dates) 
-
-def readRawHistory(uaClient = None, uaNodeId = None, starttime = None, endtime = None, numvalues = 0):
-    details = ua.ReadRawModifiedDetails()
-    details.IsReadModified = False
-    details.StartTime = starttime if starttime else ua.get_win_epoch()
-    details.EndTime = endtime if endtime else ua.get_win_epoch()
-    details.NumValuesPerNode = numvalues
-    details.ReturnBounds = True
-    
-    valueid = ua.HistoryReadValueId()
-    valueid.NodeId = uaNodeId
-    valueid.IndexRange = ''
-
-    params = ua.HistoryReadParameters()
-    params.HistoryReadDetails = details
-    params.TimestampsToReturn = ua.TimestampsToReturn.Both
-    params.ReleaseContinuationPoints = False
-    params.NodesToRead.append(valueid)
-    
-    historyReadResult = uaClient.history_read(params)[0]
-    
-    return historyReadResult
-
-def readHistory2(macId, browseName = None, startTime = None, endTime = None, numValues = 8192):
     
     # get variable handle
     nsIdx = client.get_namespace_index('http://www.iqunet.com')
@@ -96,43 +31,6 @@ def readHistory2(macId, browseName = None, startTime = None, endTime = None, num
         dates.reverse()
     return (values, dates)
     
-def readHistory3(macId, browseName = None, startTime = None, endTime = None, numValues = 8192):
-    # get variable handle
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = browseName, namespaceidx = nsIdx))  
-    uaNode = client.get_root_node().get_child(bpath)
-        
-    # read history from variable
-    dates, values, starttime, endtime = ([], [], startTime, endTime)
-    # In each iteration, 1024 samples are extracted.
-    while numValues > 0:
-        if starttime and endtime and starttime > endtime:
-            break
-        historyData = uaNode.read_raw_history(starttime = starttime, endtime = endtime, numvalues = min(numValues, 1024))
-        dvArr = historyData
-        if len(dvArr) == 0:
-            break
-        numValues -= len(dvArr)
-        for sample in dvArr:
-            dates.append(pytz.utc.localize(sample.SourceTimestamp))
-            values.append(sample.Value.Value)
-        # Set new endtime to 'oldest time stamp' minus 1us. For opcua history, timestamps
-        # used in read_raw_history(...) are ServerTimestamp, not SourceTimestamp!
-        if starttime is None:
-            # read_raw_history returns [oldest..newest]. Continue reading at 'newest - 1us'
-            endtime = pytz.utc.localize(dvArr[-1].ServerTimestamp) - datetime.timedelta(microseconds=1)
-        else:
-            # read_raw_history returns [newest..oldest]. Continue reading at 'oldest + 1us'
-            starttime = pytz.utc.localize(dvArr[-1].ServerTimestamp) + datetime.timedelta(microseconds=1)
-    # If no starttime is given, results of read_raw_history are reversed.
-    if starttime is None:
-        values.reverse()
-        dates.reverse()
-    return (values, dates) 
-    
 if __name__ == '__main__':
     
     # create client (replace xx.xx.xx.xx with the IP address of your server)
@@ -148,12 +46,6 @@ if __name__ == '__main__':
         
         # retrieve vibration data (replace xx:xx:xx:xx with the macId of your sensor)
         (values,dates) = readHistory("xx:xx:xx:xx", browseName = "accelerationPack", startTime = startDate, endTime = endDate)
-        
-        # retrieve vibration data (replace xx:xx:xx:xx with the macId of your sensor)
-        (values, dates) = readHistory2("xx:xx:xx:xx", browseName = "accelerationPack", startTime = startDate, endTime = endDate)
-        
-        # retrieve vibration data (replace xx:xx:xx:xx with the macId of your sensor)
-        (values,dates) = readHistory3("xx:xx:xx:xx", browseName = "accelerationPack", startTime = startDate, endTime = endDate)
        
         # convert vibration data to 'g' units and plot data
         data = [val[1:-6] for val in values]
