f61acb65a5b4373055d3cbfc35806f439c2a66b8
knbbl <36883064+knbbl@users.noreply.github.com>
2019-01-30 16:53:18 +0100

    Update GraphQLGetDeviceList.js

--- examples/GraphQLGetDeviceList.js
+++ examples/GraphQLGetDeviceList.js
@@ -14,52 +14,52 @@ function parseCookies(request){
 
 async function createGraphQLClient(url){
     var host = url.split('//')[1].split('/')[0];
-	  var options = {
-		  uri: url,
-		  resolveWithFullResponse: true,
-		  headers: {
-			      'Host': host,
-			      'Accept': 'text/html',
-		    }
-	  };
-	  var response = await rq.get(options);
-	  var parsedCookie = parseCookies(response);
-	  var cookie = await rq.cookie('csrftoken=' + parsedCookie['csrftoken']);
+    var options = {
+	uri: url,
+	resolveWithFullResponse: true,
+	headers: {
+            'Host': host,
+  	    'Accept': 'text/html',
+	}
+    };
+    var response = await rq.get(options);
+    var parsedCookie = parseCookies(response);
+    var cookie = await rq.cookie('csrftoken=' + parsedCookie['csrftoken']);
   
-	  const client = new GraphQLClient(
-		    url,
-		    {
-			      credentials: 'include',
-			      headers: {
-				        'x-csrftoken': parsedCookie['csrftoken'],
-				        'Cookie': cookie,
-				        'Content-Type' : 'application/json',
-				        'Accept' : 'application/json'
-			      }
-		    }
+    const client = new GraphQLClient(
+        url,
+	{
+	    credentials: 'include',
+	    headers: {
+		'x-csrftoken': parsedCookie['csrftoken'],
+		'Cookie': cookie,
+	        'Content-Type' : 'application/json',
+		'Accept' : 'application/json'
+	    }
+	}
     );
-	  return client;
+    return client;
 }
 
 async function main() {
     // create client (replace xx.xx.xx.xx with the IP address of your server)
-	  const url = 'http://xx.xx.xx.xx:8000/graphql';
-	  const queryClient = await createGraphQLClient(url);
+    const url = 'http://xx.xx.xx.xx:8000/graphql';
+    const queryClient = await createGraphQLClient(url);
 	
     // construct query
-	  const query = `
-	  {
+    const query = `
+    {
         deviceManager { 
             deviceList {
                 parent macId tag
             } 
         }
     }
-	  `;
+    `;
   
     // execute query and print device list
-	  const deviceList = await queryClient.request(query);
-	  console.log(deviceList.deviceManager.deviceList);  
+    const deviceList = await queryClient.request(query);
+    console.log(deviceList.deviceManager.deviceList);  
 }
 
 main().catch(error => console.error(error))
