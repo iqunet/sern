da22f963bcb22665e680e33a7d637f53faa5c213
knbbl <36883064+knbbl@users.noreply.github.com>
2020-02-13 14:36:09 +0100

    Update OPCuaGetDeviceList.py

--- examples/OPCuaGetDeviceList.py
+++ examples/OPCuaGetDeviceList.py
@@ -1,34 +1,156 @@
-from opcua import ua
-from opcua import Client
-        
-if __name__ == '__main__':
+import time
+import logging
+from urllib.parse import urlparse
+
+from opcua import ua, Client
+
+class OpcUaClient(object):
+    CONNECT_TIMEOUT = 15  # [sec]
+    RETRY_DELAY = 10  # [sec]
+    MAX_RETRIES = 3  # [-]
+
+    class Decorators(object):
+        @staticmethod
+        def autoConnectingClient(wrappedMethod):
+            def wrapper(obj, *args, **kwargs):
+                for retry in range(OpcUaClient.MAX_RETRIES):
+                    try:
+                        return wrappedMethod(obj, *args, **kwargs)
+                    except ua.uaerrors.BadNoMatch:
+                        raise
+                    except Exception:
+                        pass
+                    try:
+                        obj._logger.warn('(Re)connecting to OPC-UA service.')
+                        obj.reconnect()
+                    except ConnectionRefusedError:
+                        obj._logger.warn(
+                            'Connection refused. Retry in 10s.'.format(
+                                OpcUaClient.RETRY_DELAY
+                            )
+                        )
+                        time.sleep(OpcUaClient.RETRY_DELAY)
+                else:  # So the exception is exposed.
+                    obj.reconnect()
+                    return wrappedMethod(obj, *args, **kwargs)
+            return wrapper
+
+    def __init__(self, serverUrl):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._client = Client(
+            serverUrl.geturl(),
+            timeout=self.CONNECT_TIMEOUT
+        )
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+        self._client = None
+
+    @property
+    @Decorators.autoConnectingClient
+    def sensorList(self):
+        return self.objectsNode.get_children()
+
+    @property
+    @Decorators.autoConnectingClient
+    def objectsNode(self):
+        path = [ua.QualifiedName(name='Objects', namespaceidx=0)]
+        return self._client.get_root_node().get_child(path)
+
+    def connect(self):
+        self._client.connect()
+        self._client.load_type_definitions()
+
+    def disconnect(self):
+        try:
+            self._client.disconnect()
+        except Exception:
+            pass
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
+
+    @Decorators.autoConnectingClient
+    def get_browse_name(self, uaNode):
+        return uaNode.get_browse_name()
     
-    # create client (replace xx.xx.xx.xx with the IP address of your server)
-    client = Client("opc.tcp://xx.xx.xx.xx:4840/freeopcua/server/")
-    
-    try:
-        # connect client
-        client.connect()        
-        
+    @Decorators.autoConnectingClient
+    def get_node_class(self, uaNode):
+        return uaNode.get_node_class()
+
+    @Decorators.autoConnectingClient
+    def get_child(self, uaNode, path):
+        return uaNode.get_child(path)
+
+    @Decorators.autoConnectingClient
+    def read_raw_history(self,
+                         uaNode,
+                         starttime=None,
+                         endtime=None,
+                         numvalues=0,
+                         cont=None):
+        details = ua.ReadRawModifiedDetails()
+        details.IsReadModified = False
+        details.StartTime = starttime or ua.get_win_epoch()
+        details.EndTime = endtime or ua.get_win_epoch()
+        details.NumValuesPerNode = numvalues
+        details.ReturnBounds = True
+        result = OpcUaClient._history_read(uaNode, details, cont)
+        assert(result.StatusCode.is_good())
+        return result.HistoryData.DataValues, result.ContinuationPoint
+
+    @staticmethod
+    def _history_read(uaNode, details, cont):
+        valueid = ua.HistoryReadValueId()
+        valueid.NodeId = uaNode.nodeid
+        valueid.IndexRange = ''
+        valueid.ContinuationPoint = cont
+
+        params = ua.HistoryReadParameters()
+        params.HistoryReadDetails = details
+        params.TimestampsToReturn = ua.TimestampsToReturn.Both
+        params.ReleaseContinuationPoints = False
+        params.NodesToRead.append(valueid)
+        result = uaNode.server.history_read(params)[0]
+        return result
+
+class DataAcquisition(object):
+    LOGGER = logging.getLogger('DataAcquisition')
+				
+    @staticmethod
+    def get_device_list(serverUrl):
         # get device list and print result
-        nsIdx = client.get_namespace_index('http://www.iqunet.com')
-        path = [ua.QualifiedName(name = 'Objects', namespaceidx = 0)]
-        uaNode = client.get_root_node().get_child(path)
-        nodes = uaNode.get_children()
         deviceList = dict()
-        for n in nodes:
-            macId = n.get_display_name().Text
-            
-            if macId != 'Server':
-                bpath = []
-                bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-                bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-                bpath.append(ua.QualifiedName(name = "deviceTag", namespaceidx = nsIdx))  
-                uaNode2 = client.get_root_node().get_child(bpath)
-                deviceTag = uaNode2.get_value()
-                deviceList[macId] = deviceTag
+        with OpcUaClient(serverUrl) as client:
+            for sensorNode in client.sensorList:
+                macId = client.get_browse_name(sensorNode).Name
+                tagPath = ua.QualifiedName(
+                    'deviceTag',
+                    sensorNode.nodeid.NamespaceIndex
+                )
+                if (client.get_node_class(sensorNode) is ua.NodeClass.Object) and ("server" not in macId.lower()):
+                    try:
+                        deviceTag = client.get_child(sensorNode, tagPath).get_value()
+                        deviceList[macId] = '{:s}'.format(deviceTag)
+                    except Exception:
+                        deviceList[macId] = 'Device'
+                        continue
         print(deviceList)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("opcua").setLevel(logging.WARNING)
+
+    # replace xx.xx.xx.xx with the IP address of your server
+    serverIP = "xx.xx.xx.xx"
+    serverUrl = urlparse('opc.tcp://{:s}:4840'.format(serverIP))
+	
+    DataAcquisition.get_device_list(serverUrl=serverUrl)
     
-    finally:
-        # disconnect client
-        client.disconnect()
+
