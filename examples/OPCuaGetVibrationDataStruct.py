2a388a6f0114728d16c35896d12795e955e6ccde
knbbl <36883064+knbbl@users.noreply.github.com>
2020-02-17 14:35:34 +0100

    Update OPCuaGetVibrationDataStruct.py

--- examples/OPCuaGetVibrationDataStruct.py
+++ examples/OPCuaGetVibrationDataStruct.py
@@ -1,176 +1,324 @@
-from opcua import ua
-from opcua import Client
+
+import time
+import sys
+import pytz
+import logging
+import datetime
+import itertools
+from urllib.parse import urlparse
 import matplotlib.pyplot as plt
 
-def getFrequencyDomainAccelerationData(client, macId, axis):
-    
-    # axis to lower case
-    axis = axis.lower()
-    
-    # get node
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = "vibration", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis, namespaceidx = nsIdx)) 
-    bpath.append(ua.QualifiedName(name = "accel", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis + "AccelFreq", namespaceidx = nsIdx)) 
-    uaNode = client.get_root_node().get_child(bpath)
-    
-    # load vibration data
-    client.load_type_definitions()  # scan server for custom structures and import them
-    vibrationData = uaNode.get_data_value()
-    
-    # create dictionary
-    vibDict = dict()
-    vibrationStruct = vibrationData.Value.Value
-    vibDict['SourceTimestamp'] = vibrationData.SourceTimestamp
-    vibDict['ServerTimestamp'] = vibrationData.ServerTimestamp
-    vibDict['y_ordinate'] = vibrationStruct.y_ordinate
-    vibDict['x_abscissa'] = vibrationStruct.x_abscissa
-    vibDict['sampleRate'] = vibrationStruct.sampleRate
-    vibDict['formatRange'] = vibrationStruct.formatRange
-    vibDict['axis'] = vibrationStruct.axis
-    vibDict['vUnits'] = vibrationStruct.vUnits
-    vibDict['freqDomain'] = vibrationStruct.freqDomain
-            
-    return vibDict
-    
-def getTimeDomainAccelerationData(client, macId, axis):
-    
-    # axis to lower case
-    axis = axis.lower()
-    
-    # get node
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = "vibration", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis, namespaceidx = nsIdx)) 
-    bpath.append(ua.QualifiedName(name = "accel", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis + "AccelTime", namespaceidx = nsIdx)) 
-    uaNode = client.get_root_node().get_child(bpath)
-    
-    # load vibration data
-    client.load_type_definitions()  # scan server for custom structures and import them
-    vibrationData = uaNode.get_data_value()
-    
-    # create dictionary
-    vibDict = dict()
-    vibrationStruct = vibrationData.Value.Value
-    vibDict['SourceTimestamp'] = vibrationData.SourceTimestamp
-    vibDict['ServerTimestamp'] = vibrationData.ServerTimestamp
-    vibDict['y_ordinate'] = vibrationStruct.y_ordinate
-    vibDict['x_abscissa'] = vibrationStruct.x_abscissa
-    vibDict['sampleRate'] = vibrationStruct.sampleRate
-    vibDict['formatRange'] = vibrationStruct.formatRange
-    vibDict['axis'] = vibrationStruct.axis
-    vibDict['vUnits'] = vibrationStruct.vUnits
-    vibDict['freqDomain'] = vibrationStruct.freqDomain
-            
-    return vibDict
-    
-def getFrequencyDomainVelocityData(client, macId, axis):
-    
-    # axis to lower case
-    axis = axis.lower()
-    
-    # get node
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = "vibration", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis, namespaceidx = nsIdx)) 
-    bpath.append(ua.QualifiedName(name = "veloc", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis + "VelocFreq", namespaceidx = nsIdx)) 
-    uaNode = client.get_root_node().get_child(bpath)
-    
-    # load vibration data
-    client.load_type_definitions()  # scan server for custom structures and import them
-    vibrationData = uaNode.get_data_value()
-    
-    # create dictionary
-    vibDict = dict()
-    vibrationStruct = vibrationData.Value.Value
-    vibDict['SourceTimestamp'] = vibrationData.SourceTimestamp
-    vibDict['ServerTimestamp'] = vibrationData.ServerTimestamp
-    vibDict['y_ordinate'] = vibrationStruct.y_ordinate
-    vibDict['x_abscissa'] = vibrationStruct.x_abscissa
-    vibDict['sampleRate'] = vibrationStruct.sampleRate
-    vibDict['formatRange'] = vibrationStruct.formatRange
-    vibDict['axis'] = vibrationStruct.axis
-    vibDict['vUnits'] = vibrationStruct.vUnits
-    vibDict['freqDomain'] = vibrationStruct.freqDomain
-            
-    return vibDict
-    
-def getTimeDomainVelocityData(client, macId, axis):
-    
-    # axis to lower case
-    axis = axis.lower()
-    
-    # get node
-    nsIdx = client.get_namespace_index('http://www.iqunet.com')
-    bpath = []
-    bpath.append(ua.QualifiedName(name = 'Objects', namespaceidx = 0))
-    bpath.append(ua.QualifiedName(name = macId, namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = "vibration", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis, namespaceidx = nsIdx)) 
-    bpath.append(ua.QualifiedName(name = "veloc", namespaceidx = nsIdx))
-    bpath.append(ua.QualifiedName(name = axis + "VelocTime", namespaceidx = nsIdx)) 
-    uaNode = client.get_root_node().get_child(bpath)
-    
-    # load vibration data
-    client.load_type_definitions()  # scan server for custom structures and import them
-    vibrationData = uaNode.get_data_value()
-    
-    # create dictionary
-    vibDict = dict()
-    vibrationStruct = vibrationData.Value.Value
-    vibDict['SourceTimestamp'] = vibrationData.SourceTimestamp
-    vibDict['ServerTimestamp'] = vibrationData.ServerTimestamp
-    vibDict['y_ordinate'] = vibrationStruct.y_ordinate
-    vibDict['x_abscissa'] = vibrationStruct.x_abscissa
-    vibDict['sampleRate'] = vibrationStruct.sampleRate
-    vibDict['formatRange'] = vibrationStruct.formatRange
-    vibDict['axis'] = vibrationStruct.axis
-    vibDict['vUnits'] = vibrationStruct.vUnits
-    vibDict['freqDomain'] = vibrationStruct.freqDomain
-            
-    return vibDict
-    
-if __name__ == '__main__':
-    
-    # create client (replace xx.xx.xx.xx with the IP address of your server)
-    client = Client("opc.tcp://xx.xx.xx.xx:4840/freeopcua/server/")
-    
-    try:
-        # connect client
-        client.connect()        
-        
-        # retrieve vibration data (replace xx:xx:xx:xx with the macId of your sensor and select the x, y or z axis)
-        freqAcc = getFrequencyDomainAccelerationData(client, "xx:xx:xx:xx", "x")
-        timeAcc = getTimeDomainAccelerationData(client, "xx:xx:xx:xx", "x")
-        freqVel = getFrequencyDomainVelocityData(client, "xx:xx:xx:xx", "x")
-        timeVel = getTimeDomainVelocityData(client, "xx:xx:xx:xx", "x")
-        
-        # plot vibration data
+from opcua import ua, Client
+
+
+class OpcUaClient(object):
+    CONNECT_TIMEOUT = 15  # [sec]
+    RETRY_DELAY = 10  # [sec]
+    MAX_RETRIES = 3  # [-]
+
+    class Decorators(object):
+        @staticmethod
+        def autoConnectingClient(wrappedMethod):
+            def wrapper(obj, *args, **kwargs):
+                for retry in range(OpcUaClient.MAX_RETRIES):
+                    try:
+                        return wrappedMethod(obj, *args, **kwargs)
+                    except ua.uaerrors.BadNoMatch:
+                        raise
+                    except Exception:
+                        pass
+                    try:
+                        obj._logger.warn('(Re)connecting to OPC-UA service.')
+                        obj.reconnect()
+                    except ConnectionRefusedError:
+                        obj._logger.warn(
+                            'Connection refused. Retry in 10s.'.format(
+                                OpcUaClient.RETRY_DELAY
+                            )
+                        )
+                        time.sleep(OpcUaClient.RETRY_DELAY)
+                else:  # So the exception is exposed.
+                    obj.reconnect()
+                    return wrappedMethod(obj, *args, **kwargs)
+            return wrapper
+
+    def __init__(self, serverUrl):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._client = Client(
+            serverUrl.geturl(),
+            timeout=self.CONNECT_TIMEOUT
+        )
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+        self._client = None
+
+    @property
+    @Decorators.autoConnectingClient
+    def sensorList(self):
+        return self.objectsNode.get_children()
+
+    @property
+    @Decorators.autoConnectingClient
+    def objectsNode(self):
+        path = [ua.QualifiedName(name='Objects', namespaceidx=0)]
+        return self._client.get_root_node().get_child(path)
+
+    def connect(self):
+        self._client.connect()
+        self._client.load_type_definitions()
+
+    def disconnect(self):
+        try:
+            self._client.disconnect()
+        except Exception:
+            pass
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
+
+    @Decorators.autoConnectingClient
+    def get_browse_name(self, uaNode):
+        return uaNode.get_browse_name()
+
+    @Decorators.autoConnectingClient
+    def get_node_class(self, uaNode):
+        return uaNode.get_node_class()
+
+    @Decorators.autoConnectingClient
+    def get_namespace_index(self, uri):
+        return self._client.get_namespace_index(uri)
+
+    @Decorators.autoConnectingClient
+    def get_child(self, uaNode, path):
+        return uaNode.get_child(path)
+
+    @Decorators.autoConnectingClient
+    def read_raw_history(self,
+                         uaNode,
+                         starttime=None,
+                         endtime=None,
+                         numvalues=0,
+                         cont=None):
+        details = ua.ReadRawModifiedDetails()
+        details.IsReadModified = False
+        details.StartTime = starttime or ua.get_win_epoch()
+        details.EndTime = endtime or ua.get_win_epoch()
+        details.NumValuesPerNode = numvalues
+        details.ReturnBounds = True
+        result = OpcUaClient._history_read(uaNode, details, cont)
+        assert(result.StatusCode.is_good())
+        return result.HistoryData.DataValues, result.ContinuationPoint
+
+    @staticmethod
+    def _history_read(uaNode, details, cont):
+        valueid = ua.HistoryReadValueId()
+        valueid.NodeId = uaNode.nodeid
+        valueid.IndexRange = ''
+        valueid.ContinuationPoint = cont
+
+        params = ua.HistoryReadParameters()
+        params.HistoryReadDetails = details
+        params.TimestampsToReturn = ua.TimestampsToReturn.Both
+        params.ReleaseContinuationPoints = False
+        params.NodesToRead.append(valueid)
+        result = uaNode.server.history_read(params)[0]
+        return result
+
+
+class DataAcquisition(object):
+    LOGGER = logging.getLogger('DataAcquisition')
+    AXES = ('x', 'y', 'z')
+    ORDINATES = ('accel', 'veloc')
+    DOMAINS = ('time', 'freq')
+    MAX_VALUES_PER_ENDNODE = 100  # Num values per endnode
+    MAX_VALUES_PER_REQUEST = 2  # Num values per history request
+
+    @staticmethod
+    def selected_to_workbook(serverUrl,
+                             macIdsToCollect,
+                             starttime,
+                             endtime):
+        with OpcUaClient(serverUrl) as client:
+            for sensorNode in client.sensorList:
+                assert(client._client.uaclient._uasocket.timeout == 15)
+                macId = client.get_browse_name(sensorNode).Name
+                if macId not in macIdsToCollect:
+                    DataAcquisition.LOGGER.info(
+                        'Skipping sensor {:s}'.format(macId)
+                    )
+                    continue
+                tagPath = ua.QualifiedName(
+                    'deviceTag',
+                    sensorNode.nodeid.NamespaceIndex
+                )
+                DataAcquisition.LOGGER.info(
+                    'Processing sensor {:s} ({:s})'.format(
+                        macId,
+                        client.get_child(sensorNode, tagPath).get_value()
+                    )
+                )
+                DataAcquisition.get_sensor_data(
+                        client,
+                        sensorNode,
+                        starttime,
+                        endtime
+                )
+
+    @staticmethod
+    def get_sensor_data(serverUrl, macId, browseName, starttime, endtime):
+        allValues = []
+        allDates = []
+        with OpcUaClient(serverUrl) as client:
+            assert(client._client.uaclient._uasocket.timeout == 15)
+            sensorNode = DataAcquisition.get_sensor_node(
+                    client,
+                    macId,
+                    browseName
+            )
+            for path in DataAcquisition.endnodes_path_generator(sensorNode):
+                DataAcquisition.LOGGER.info(
+                        'Browsing {:s} -> {:s}'.format(
+                                macId,
+                                sensorNode.get_browse_name().Name
+                            )
+                )
+                endNode = client.get_child(sensorNode, path)
+                (values, dates) = DataAcquisition.get_endnode_data(
+                        client,
+                        endNode,
+                        starttime,
+                        endtime
+                )
+                allValues.extend(values)
+                allDates.extend(dates)
+        return (allValues, allDates)
+
+    @staticmethod
+    def endnodes_path_generator(sensorNode):
+        for (axis, ordinate, domain) in \
+                itertools.product(DataAcquisition.AXES,
+                                  DataAcquisition.ORDINATES,
+                                  DataAcquisition.DOMAINS):
+            # browseName: e.g. xAccelTime
+            browseName = ''.join([
+                axis, ordinate.capitalize(), domain.capitalize()
+            ])
+            nsIdx = sensorNode.nodeid.NamespaceIndex  # iQunet namespace index
+            path = [
+                ua.QualifiedName(axis, nsIdx),        # e.g. 'x'
+                ua.QualifiedName(ordinate, nsIdx),    # e.g. 'accel'
+                ua.QualifiedName(browseName, nsIdx),  # e.g. 'xAccelTime'
+            ]
+            yield path
+
+    @staticmethod
+    def get_sensor_node(client, macId, browseName):
+        nsIdx = client.get_namespace_index(
+                'http://www.iqunet.com'
+        )  # iQunet namespace index
+        bpath = [
+                ua.QualifiedName(name=macId, namespaceidx=nsIdx),
+                ua.QualifiedName(name=browseName, namespaceidx=nsIdx)
+                ]
+        sensorNode = client.objectsNode.get_child(bpath)
+        return sensorNode
+
+    @staticmethod
+    def get_endnode_data(client, endNode, starttime, endtime):
+        dvList = DataAcquisition.download_endnode(
+                client,
+                endNode,
+                starttime,
+                endtime
+        )
+        dates, values = ([], [])
+        for dv in dvList:
+            dates.append(dv.SourceTimestamp.strftime('%Y-%m-%d %H:%M:%S'))
+            values.append(dv.Value.Value.y_ordinate)
+
+        # If no starttime is given, results of read_raw_history are reversed.
+        if starttime is None:
+            values.reverse()
+            dates.reverse()
+        return (values, dates)
+
+    @staticmethod
+    def download_endnode(client, endNode, starttime, endtime):
+        endNodeName = client.get_browse_name(endNode).Name
+        DataAcquisition.LOGGER.info(
+                'Downloading endnode {:s}'.format(
+                        endNodeName
+                    )
+        )
+        dvList, contId = [], None
+        while True:
+            remaining = DataAcquisition.MAX_VALUES_PER_ENDNODE - len(dvList)
+            assert(remaining >= 0)
+            numvalues = min(DataAcquisition.MAX_VALUES_PER_REQUEST, remaining)
+            partial, contId = client.read_raw_history(
+                uaNode=endNode,
+                starttime=starttime,
+                endtime=endtime,
+                numvalues=numvalues,
+                cont=contId
+            )
+            if not len(partial):
+                DataAcquisition.LOGGER.warn(
+                    'No data was returned for {:s}'.format(endNodeName)
+                )
+                break
+            dvList.extend(partial)
+            sys.stdout.write('\r    Loaded {:d} values, {:s} -> {:s}'.format(
+                len(dvList),
+                str(dvList[0].ServerTimestamp.strftime("%Y-%m-%d %H:%M:%S")),
+                str(dvList[-1].ServerTimestamp.strftime("%Y-%m-%d %H:%M:%S"))
+            ))
+            sys.stdout.flush()
+            if contId is None:
+                break  # No more data.
+            if len(dvList) >= DataAcquisition.MAX_VALUES_PER_ENDNODE:
+                break  # Too much data.
+        sys.stdout.write('...OK.\n')
+        return dvList
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("opcua").setLevel(logging.WARNING)
+
+    # replace xx.xx.xx.xx with the IP address of your server
+    serverIP = "xx.xx.xx.xx"
+    serverUrl = urlparse('opc.tcp://{:s}:4840'.format(serverIP))
+
+    # replace xx:xx:xx:xx with your sensors macId
+    macId = 'xx:xx:xx:x'
+
+    starttime = pytz.utc.localize(
+        datetime.datetime.strptime("2020-02-01 00:00:00", '%Y-%m-%d %H:%M:%S')
+    )
+    endtime = pytz.utc.localize(
+        datetime.datetime.strptime("2020-02-24 00:00:00", '%Y-%m-%d %H:%M:%S')
+    )
+
+    # acquire history data
+    (values, dates) = DataAcquisition.get_sensor_data(
+        serverUrl=serverUrl,
+        macId=macId,
+        browseName="vibration",
+        starttime=starttime,
+        endtime=endtime
+    )
+
+    # convert vibration data to 'g' units and plot data
+    data = [val[1:-6] for val in values]
+    formatRanges = [val[-5] for val in values]
+    for i in range(len(formatRanges)):
         plt.figure()
-        plt.plot(freqAcc['x_abscissa'],freqAcc['y_ordinate'])
-        plt.title("AccelFreq")
-        plt.figure()
-        plt.plot(timeAcc['x_abscissa'],timeAcc['y_ordinate'])
-        plt.title("AccelTime")
-        plt.figure()
-        plt.plot(freqVel['x_abscissa'],freqVel['y_ordinate'])
-        plt.title("VelocFreq")
-        plt.figure()
-        plt.plot(timeVel['x_abscissa'],timeVel['y_ordinate'])
-        plt.title("VelocTime")    
-                
-    finally:
-        # disconnect client
-        client.disconnect()
+        plt.plot(data[i])
+        plt.title(str(dates[i]))
