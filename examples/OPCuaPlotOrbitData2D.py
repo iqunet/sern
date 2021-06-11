bf3dc354b74f62d39aced6ddae5fbc4f509e5608
knbbl <36883064+knbbl@users.noreply.github.com>
2021-06-11 16:04:58 +0200

    Create OPCuaPlotOrbitData2D.py

---
+++ examples/OPCuaPlotOrbitData2D.py
@@ -0,0 +1,344 @@
+import time
+import sys
+import pytz
+import logging
+import datetime
+from urllib.parse import urlparse
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal
+
+from opcua import ua, Client
+
+class HighPassFilter(object):
+
+    @staticmethod
+    def get_highpass_coefficients(lowcut, sampleRate, order=5):
+        nyq = 0.5 * sampleRate
+        low = lowcut / nyq
+        b, a = scipy.signal.butter(order, [low], btype='highpass')
+        return b, a
+
+    @staticmethod
+    def run_highpass_filter(data, lowcut, sampleRate, order=5):
+        if lowcut >= sampleRate/2.0:
+            return data*0.0
+        b, a = HighPassFilter.get_highpass_coefficients(lowcut, sampleRate, order=order)
+        y = scipy.signal.filtfilt(b, a, data, padtype='even')
+        return y
+    
+    @staticmethod
+    def perform_hpf_filtering(data, sampleRate, hpf=3):
+        if hpf == 0:
+            return data
+        data[0:6] = data[13:7:-1] # skip compressor settling
+        data = HighPassFilter.run_highpass_filter(
+            data=data,
+            lowcut=3,
+            sampleRate=sampleRate,
+            order=1,
+        )
+        data = HighPassFilter.run_highpass_filter(
+            data=data,
+            lowcut=int(hpf),
+            sampleRate=sampleRate,
+            order=2,
+        )
+        return data
+
+class OpcUaClient(object):
+    CONNECT_TIMEOUT = 15  # [sec]
+    RETRY_DELAY = 10  # [sec]
+    MAX_RETRIES = 3  # [-]
+
+    class Decorators(object):
+        @staticmethod
+        def autoConnectingClient(wrappedMethod):
+            def wrapper(obj, *args, **kwargs):
+                for retry in range(OpcUaClient.MAX_RETRIES):
+                    try:
+                        return wrappedMethod(obj, *args, **kwargs)
+                    except ua.uaerrors.BadNoMatch:
+                        raise
+                    except Exception:
+                        pass
+                    try:
+                        obj._logger.warn('(Re)connecting to OPC-UA service.')
+                        obj.reconnect()
+                    except ConnectionRefusedError:
+                        obj._logger.warn(
+                            'Connection refused. Retry in 10s.'.format(
+                                OpcUaClient.RETRY_DELAY
+                            )
+                        )
+                        time.sleep(OpcUaClient.RETRY_DELAY)
+                else:  # So the exception is exposed.
+                    obj.reconnect()
+                    return wrappedMethod(obj, *args, **kwargs)
+            return wrapper
+
+    def __init__(self, serverUrl):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._client = Client(
+            serverUrl.geturl(),
+            timeout=self.CONNECT_TIMEOUT
+        )
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+        self._client = None
+
+    @property
+    @Decorators.autoConnectingClient
+    def sensorList(self):
+        return self.objectsNode.get_children()
+
+    @property
+    @Decorators.autoConnectingClient
+    def objectsNode(self):
+        path = [ua.QualifiedName(name='Objects', namespaceidx=0)]
+        return self._client.get_root_node().get_child(path)
+
+    def connect(self):
+        self._client.connect()
+        self._client.load_type_definitions()
+
+    def disconnect(self):
+        try:
+            self._client.disconnect()
+        except Exception:
+            pass
+
+    def reconnect(self):
+        self.disconnect()
+        self.connect()
+
+    @Decorators.autoConnectingClient
+    def get_browse_name(self, uaNode):
+        return uaNode.get_browse_name()
+
+    @Decorators.autoConnectingClient
+    def get_node_class(self, uaNode):
+        return uaNode.get_node_class()
+
+    @Decorators.autoConnectingClient
+    def get_namespace_index(self, uri):
+        return self._client.get_namespace_index(uri)
+
+    @Decorators.autoConnectingClient
+    def get_child(self, uaNode, path):
+        return uaNode.get_child(path)
+
+    @Decorators.autoConnectingClient
+    def read_raw_history(self,
+                         uaNode,
+                         starttime=None,
+                         endtime=None,
+                         numvalues=0,
+                         cont=None):
+        details = ua.ReadRawModifiedDetails()
+        details.IsReadModified = False
+        details.StartTime = starttime or ua.get_win_epoch()
+        details.EndTime = endtime or ua.get_win_epoch()
+        details.NumValuesPerNode = numvalues
+        details.ReturnBounds = True
+        result = OpcUaClient._history_read(uaNode, details, cont)
+        assert(result.StatusCode.is_good())
+        return result.HistoryData.DataValues, result.ContinuationPoint
+
+    @staticmethod
+    def _history_read(uaNode, details, cont):
+        valueid = ua.HistoryReadValueId()
+        valueid.NodeId = uaNode.nodeid
+        valueid.IndexRange = ''
+        valueid.ContinuationPoint = cont
+
+        params = ua.HistoryReadParameters()
+        params.HistoryReadDetails = details
+        params.TimestampsToReturn = ua.TimestampsToReturn.Both
+        params.ReleaseContinuationPoints = False
+        params.NodesToRead.append(valueid)
+        result = uaNode.server.history_read(params)[0]
+        return result
+
+class DataAcquisition(object):
+    LOGGER = logging.getLogger('DataAcquisition')
+    MAX_VALUES_PER_ENDNODE = 1000  # Num values per endnode
+    MAX_VALUES_PER_REQUEST = 10  # Num values per history request
+
+    @staticmethod
+    def get_sensor_data(serverUrl, macId, browseName, starttime, endtime):
+        with OpcUaClient(serverUrl) as client:
+            assert(client._client.uaclient._uasocket.timeout == 15)
+            sensorNode = \
+                DataAcquisition.get_sensor_node(client, macId, browseName)
+            DataAcquisition.LOGGER.info(
+                    'Browsing {:s}'.format(macId)
+            )
+            (values, dates) = \
+                DataAcquisition.get_endnode_data(
+                        client=client,
+                        endNode=sensorNode,
+                        starttime=starttime,
+                        endtime=endtime
+                )
+        return (values, dates)
+
+    @staticmethod
+    def get_sensor_node(client, macId, browseName):
+        nsIdx = client.get_namespace_index(
+                'http://www.iqunet.com'
+        )  # iQunet namespace index
+        bpath = [
+                ua.QualifiedName(name=macId, namespaceidx=nsIdx),
+                ua.QualifiedName(name=browseName, namespaceidx=nsIdx)
+        ]
+        sensorNode = client.objectsNode.get_child(bpath)
+        return sensorNode
+
+    @staticmethod
+    def get_endnode_data(client, endNode, starttime, endtime):
+        dvList = DataAcquisition.download_endnode(
+                client=client,
+                endNode=endNode,
+                starttime=starttime,
+                endtime=endtime
+        )
+        dates, values = ([], [])
+        for dv in dvList:
+            dates.append(dv.SourceTimestamp.strftime('%Y-%m-%d %H:%M:%S.%f'))
+            values.append(dv.Value.Value)
+
+        # If no starttime is given, results of read_raw_history are reversed.
+        if starttime is None:
+            values.reverse()
+            dates.reverse()
+        return (values, dates)
+
+    @staticmethod
+    def download_endnode(client, endNode, starttime, endtime):
+        endNodeName = client.get_browse_name(endNode).Name
+        DataAcquisition.LOGGER.info(
+                'Downloading endnode {:s}'.format(
+                    endNodeName
+                )
+        )
+        dvList, contId = [], None
+        while True:
+            remaining = DataAcquisition.MAX_VALUES_PER_ENDNODE - len(dvList)
+            assert(remaining >= 0)
+            numvalues = min(DataAcquisition.MAX_VALUES_PER_REQUEST, remaining)
+            partial, contId = client.read_raw_history(
+                uaNode=endNode,
+                starttime=starttime,
+                endtime=endtime,
+                numvalues=numvalues,
+                cont=contId
+            )
+            if not len(partial):
+                DataAcquisition.LOGGER.warn(
+                    'No data was returned for {:s}'.format(endNodeName)
+                )
+                break
+            dvList.extend(partial)
+            sys.stdout.write('\r    Loaded {:d} values, {:s} -> {:s}'.format(
+                len(dvList),
+                str(dvList[0].ServerTimestamp.strftime("%Y-%m-%d %H:%M:%S")),
+                str(dvList[-1].ServerTimestamp.strftime("%Y-%m-%d %H:%M:%S"))
+            ))
+            sys.stdout.flush()
+            if contId is None:
+                break  # No more data.
+            if len(dvList) >= DataAcquisition.MAX_VALUES_PER_ENDNODE:
+                break  # Too much data.
+        sys.stdout.write('...OK.\n')
+        return dvList
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("opcua").setLevel(logging.WARNING)
+
+    # replace xx.xx.xx.xx with the IP address of your server
+    serverIP = "xx.xx.xx.xx"
+    serverUrl = urlparse('opc.tcp://{:s}:4840'.format(serverIP))
+    
+    # replace xx:xx:xx:xx with your sensors macId
+    macId = 'xx:xx:xx:xx'
+    
+    # change settings
+    hpf = 0 # high pass filter (Hz)
+    startTime = "2021-05-15 00:00:00" # start time
+    endTime = "2021-05-25 10:00:00" # end time
+    timeZone = "Europe/Brussels" # local time zone
+    selectedAxes = [0, 1] # select 2 axes (X=0, Y=1, Z=2)
+    
+    # only select the two first chosen axes
+    if len(selectedAxes) > 2:
+        selectedAxes = selectedAxes[0:2]
+    
+    # format start and end time
+    starttime = pytz.utc.localize(
+        datetime.datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
+    )
+    endtime = pytz.utc.localize(
+        datetime.datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+    )
+    
+    # acquire history data
+    (values, dates) = DataAcquisition.get_sensor_data(
+        serverUrl=serverUrl,
+        macId=macId,
+        browseName="accelerationPack",
+        starttime=starttime,
+        endtime=endtime
+    )
+
+    # convert vibration data to 'g' units and plot data if axis in selected axes list
+    data = [val[1:-6] for val in values]
+    sampleRates = [val[-6] for val in values]
+    formatRanges = [val[-5] for val in values]
+    axes = [val[-3] for val in values]
+    
+    selectedData = []
+    selectedDates = []
+    for i in range(len(formatRanges)):
+        if axes[i] in selectedAxes:
+            data[i] = [d/512.0*formatRanges[i] for d in data[i]]
+            data[i] = HighPassFilter.perform_hpf_filtering(
+                data=data[i],
+                sampleRate=sampleRates[i], 
+                hpf=hpf
+            )
+            selectedData.append(data[i])
+            selectedDates.append(datetime.datetime.strptime(dates[i], '%Y-%m-%d %H:%M:%S.%f'))
+          
+    # sort matching axes pairs
+    duration = [((selectedDates[i]-selectedDates[i-1]).total_seconds())*1000 for i in range(1,len(selectedDates))]
+    duration = np.array(duration)
+    idx = np.where(duration < 10)[0]
+    pairs = [[ind, ind+1] for ind in idx]
+    
+    # plot matching axes pairs
+    axesTitle = str(selectedAxes[0]) + "-" + str(selectedAxes[1])
+    axesTitle = axesTitle.replace('0','X')
+    axesTitle = axesTitle.replace('1','Y')
+    axesTitle = axesTitle.replace('2','Z')
+    
+    for p in pairs:
+        if len(data[p[0]]) == len(data[p[1]]):
+            plt.figure()
+            plt.plot(data[p[0]],data[p[1]])
+            dateTitle = selectedDates[p[0]].replace(tzinfo=pytz.timezone('UTC')).astimezone(pytz.timezone(timeZone))
+            dateTitle = dateTitle.strftime("%a, %b %d, %Y %I:%M %p")
+            plt.suptitle("ACC | SCATTER " + axesTitle, fontsize=12)
+            plt.title(dateTitle + " | axis: " + axesTitle, fontsize=10)
+            plt.xlabel(axesTitle[0] + ' [g]')
+            plt.ylabel(axesTitle[2] + ' [g]')
+        else:
+            pass
+          
