702af59f0401e5b9944e055e48d85d7d1043b555
knbbl <36883064+knbbl@users.noreply.github.com>
2022-02-15 11:10:26 +0100

    Update OPCuaPlotOrbitData2D.py

--- examples/OPCuaPlotOrbitData2D.py
+++ examples/OPCuaPlotOrbitData2D.py
@@ -63,10 +63,10 @@ class OpcUaClient(object):
                     except Exception:
                         pass
                     try:
-                        obj._logger.warn('(Re)connecting to OPC-UA service.')
+                        obj._logger.warning('(Re)connecting to OPC-UA service.')
                         obj.reconnect()
                     except ConnectionRefusedError:
-                        obj._logger.warn(
+                        obj._logger.warning(
                             'Connection refused. Retry in 10s.'.format(
                                 OpcUaClient.RETRY_DELAY
                             )
@@ -240,7 +240,7 @@ class DataAcquisition(object):
                 cont=contId
             )
             if not len(partial):
-                DataAcquisition.LOGGER.warn(
+                DataAcquisition.LOGGER.warning(
                     'No data was returned for {:s}'.format(endNodeName)
                 )
                 break
